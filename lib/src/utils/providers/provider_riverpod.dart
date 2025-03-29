8fca10c048fa04b07e1e81bf7499da954742436a
StarKzH <88117244+guilhermeSchiochet@users.noreply.github.com>
2025-03-29 17:33:20 -0300

    Alteração no provider para carregar a lista de ItemModel

--- lib/src/utils/providers/provider_riverpod.dart
+++ lib/src/utils/providers/provider_riverpod.dart
@@ -1,4 +1,5 @@
 import 'package:riverpod/riverpod.dart';
+import 'package:saboreie/src/data/model/slide_item.model.dart';
 import 'package:saboreie/src/presentation/controller/home_page.controller.dart';
 
-final homePageControllerProvider = StateNotifierProvider<HomePageController, int>((ref) => HomePageController());
+final homePageControllerProvider = StateNotifierProvider<HomePageController, AsyncValue<List<SlideItemModel>>>((ref) => HomePageController());
