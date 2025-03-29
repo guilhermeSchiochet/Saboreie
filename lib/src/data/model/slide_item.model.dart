b523b34a3b88eeacf74712dd06d03547c52df09d
StarKzH <88117244+guilhermeSchiochet@users.noreply.github.com>
2025-03-29 17:32:16 -0300

    Criado model para o SlideItemModel

---
+++ lib/src/data/model/slide_item.model.dart
@@ -0,0 +1,8 @@
+class SlideItemModel {
+  final String img;
+  final String title;
+  final String description;
+  final double rating;
+
+  const SlideItemModel({required this.img, required this.title, required this.description, required this.rating});
+}
