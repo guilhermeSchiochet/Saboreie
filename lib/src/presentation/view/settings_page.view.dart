2d4b179b04266ca0354f8f0ba73ae7d7dc6ea826
StarKzH <88117244+guilhermeSchiochet@users.noreply.github.com>
2025-04-03 21:33:22 -0300

    Atualização na tela de configurações do usuario

--- lib/src/presentation/view/settings_page.view.dart
+++ lib/src/presentation/view/settings_page.view.dart
@@ -1,24 +1,84 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:saboreie/src/presentation/widgets/search_card.dart';
-import 'package:saboreie/src/utils/providers/provider_riverpod.dart';
+import 'package:lucide_icons/lucide_icons.dart';
+import 'package:saboreie/src/config/theme/app_theme.dart';
+import 'package:settings_ui/settings_ui.dart';
 
 class SettingsPageView extends ConsumerWidget {
   const SettingsPageView({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-  return Scaffold(
-      body: _body()
+    return Scaffold(
+      body: _body(),
     );
   }
 
   Widget _body() {
     return Column(
       children: [
-        
+        _buildContainerAvatar(),
+        const SizedBox(height: 50),
+        Expanded(
+          child: SettingsList(
+            sections: [
+              SettingsSection(
+                title: const Text('Configurações'),
+                tiles: <SettingsTile>[
+                  SettingsTile.navigation(
+                    leading: const Icon(
+                      Icons.color_lens_outlined
+                    ),
+                    title: const Text('Tema'),
+                    value: const Text('Claro'),
+                  ),
+                  SettingsTile.switchTile(
+                    onToggle: (value) {},
+                    initialValue: true,
+                    leading: const Icon(Icons.format_paint),
+                    title: const Text('Enable custom theme'),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ],
     );
   }
 
+  Widget _buildContainerAvatar() {
+    return Center(
+      child: GestureDetector(
+        onTap: () {},
+        child: const Stack(
+          clipBehavior: Clip.none,
+          children: [
+            CircleAvatar(
+              radius: 80,
+              backgroundColor: Colors.grey,
+              child: Icon(
+                Icons.person,
+                color: Colors.white,
+                size: 50
+              ),
+            ),
+            Positioned(
+              bottom: -5,
+              right: 10,
+              child: CircleAvatar(
+                radius: 18,
+                backgroundColor: AppTheme.defaultLightColor,
+                child: Icon(
+                  LucideIcons.camera,
+                  color: Colors.white,
+                  size: 24
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
